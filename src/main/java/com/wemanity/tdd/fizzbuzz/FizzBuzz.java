6182354683afc6297e3a89a6ca4506453f419be1
rebzani <a.rebzani@lescomptoirs.com>
2016-07-12 00:09:24 +0200

    TDD Update : removing binary folder pom : upgrade junit, and runing with java 1.7 --> next commit with 1.8 TDD refactoring

--- src/main/java/com/wemanity/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/wemanity/tdd/fizzbuzz/FizzBuzz.java
@@ -1,27 +1,34 @@
 package com.wemanity.tdd.fizzbuzz;
 
-/**
- * FizzBuzz
- *
- */
 public class FizzBuzz {
+    private final String FIZZ = "Fizz";
+    private final String BUZZ = "Buzz";
+    private final int FIVE = 5;
+    private final int TREE = 3;
 
-	public String evaluate(int i) {
-		StringBuffer result = new StringBuffer();
-		if (aMultipleOfb(i, 3)) {
-			result.append("Fizz");
-		}
-		if (aMultipleOfb(i, 5)) {
-			result.append("Buzz");
-		}
-		if (result.toString().isEmpty()) {
-			result.append(i);
-		}
+    public String evaluate(int incomingFizzBuzzValue) {
+        StringBuffer result = new StringBuffer();
+        if (isMultibleOf3(incomingFizzBuzzValue)) {
+            result.append(FIZZ);
+        }
+        if (isMultibleOf5(incomingFizzBuzzValue)) {
+            result.append(BUZZ);
+        }
+        if (result.length() == 0) {
+            result.append(incomingFizzBuzzValue);
+        }
 
-		return result.toString();
-	}
+        return result.toString();
+    }
+    private boolean isMultibleOf3(int value){
+        return aMultipleOfB(value, TREE);
+    }
+    
+    private boolean isMultibleOf5(int value){
+        return aMultipleOfB(value, FIVE);
+    }
 
-	private boolean aMultipleOfb(int a, int b) {
-		return (a % b) == 0;
-	}
+    private boolean aMultipleOfB(int a, int b) {
+        return (a % b) == 0;
+    }
 }
--- src/test/java/com/wemanity/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/wemanity/tdd/fizzbuzz/FizzBuzzTest.java
@@ -5,47 +5,35 @@ package com.wemanity.tdd.fizzbuzz;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- * @author uroot
- *
- */
 public class FizzBuzzTest {
-	private FizzBuzz fb;
+	private static FizzBuzz fizzBuzz;
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUpBeforeClass() throws Exception {
-		fb = new FizzBuzz();
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		fizzBuzz = new FizzBuzz();
 	}
 
 	@Test
 	public void evaluateOne() {
-		assertEquals("1", fb.evaluate(1));
+		assertEquals("1", fizzBuzz.evaluate(1));
 	}
 
 	@Test
 	public void evaluateFizz() {
-		assertEquals("Fizz", fb.evaluate(3));
+		assertEquals("Fizz", fizzBuzz.evaluate(3));
 	}
 
 	@Test
 	public void evaluateBuzz() {
-		assertEquals("Buzz", fb.evaluate(5));
-	}
-
-	@Test
-	public void evaluateFizzBuzz() {
-		assertEquals("FizzBuzz", fb.evaluate(30));
+		assertEquals("Buzz", fizzBuzz.evaluate(5));
 	}
 
 	@Test
 	public void valuateFizzBuzz() {
-		assertEquals("FizzBuzz", fb.evaluate(15));
+		assertEquals("FizzBuzz", fizzBuzz.evaluate(15));
 
 	}
 
