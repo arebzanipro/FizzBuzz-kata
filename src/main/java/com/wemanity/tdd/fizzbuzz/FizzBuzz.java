f952d71694ed43d2ec8b35ba4fe562a32bc2080e
Amir REBZANI <a.rebzani@lescomptoirs.com>
2016-07-13 19:12:55 +0200

    refactoring to understanding methods

--- src/main/java/com/wemanity/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/wemanity/tdd/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,8 @@
 package com.wemanity.tdd.fizzbuzz;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class FizzBuzz {
     private final String FIZZ = "Fizz";
     private final String BUZZ = "Buzz";
@@ -7,19 +10,39 @@ public class FizzBuzz {
     private final int TREE = 3;
 
     public String evaluate(int incomingFizzBuzzValue) {
-        StringBuffer result = new StringBuffer();
-        if (isMultibleOf3(incomingFizzBuzzValue)) {
-            result.append(FIZZ);
-        }
-        if (isMultibleOf5(incomingFizzBuzzValue)) {
-            result.append(BUZZ);
-        }
-        if (result.length() == 0) {
-            result.append(incomingFizzBuzzValue);
-        }
-
+        StringBuffer result = new StringBuffer();        
+        appendFizzIfMultipleOf3(incomingFizzBuzzValue, result);
+        appendBuzzIfMultipleOf5(incomingFizzBuzzValue, result);
+        appendNumberIfNotMultipleOf3And5(incomingFizzBuzzValue, result);
         return result.toString();
     }
+    
+    public String evaluate(List incomingListToFizzBuzz){
+    	Iterator iterator = incomingListToFizzBuzz.iterator();
+    	while(iterator.hasNext()){
+    		evaluate((int) iterator.next());
+    	}
+		return BUZZ;
+    	
+    }
+    
+    private void appendFizzIfMultipleOf3(int incomingFizzBuzzValue, StringBuffer result){
+    	if (isMultibleOf3(incomingFizzBuzzValue)) {
+            result.append(FIZZ);
+        }
+    }
+    private void appendBuzzIfMultipleOf5(int incomingFizzBuzzValue, StringBuffer result){
+    	if (isMultibleOf5(incomingFizzBuzzValue)) {
+            result.append(BUZZ);
+        }
+    }
+    
+    private void appendNumberIfNotMultipleOf3And5(int incomingFizzBuzzValue, StringBuffer result){
+    	if (result.length() == 0) {
+            result.append(incomingFizzBuzzValue);
+        }
+    }
+    
     private boolean isMultibleOf3(int value){
         return aMultipleOfB(value, TREE);
     }
@@ -31,4 +54,7 @@ public class FizzBuzz {
     private boolean aMultipleOfB(int a, int b) {
         return (a % b) == 0;
     }
+    
+    
+    
 }
--- src/test/java/com/wemanity/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/wemanity/tdd/fizzbuzz/FizzBuzzTest.java
@@ -5,36 +5,44 @@ package com.wemanity.tdd.fizzbuzz;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
-	private static FizzBuzz fizzBuzz;
+	private String[] values = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz"};
+	
+	private FizzBuzz fizzBuzz;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	@Before
+	public void setUpBeforeClass() throws Exception {
 		fizzBuzz = new FizzBuzz();
 	}
 
 	@Test
-	public void evaluateOne() {
+	public void whenNotMultiple3And5() {
 		assertEquals("1", fizzBuzz.evaluate(1));
 	}
 
 	@Test
-	public void evaluateFizz() {
+	public void whenMultiple3ReturnFizz() {
 		assertEquals("Fizz", fizzBuzz.evaluate(3));
 	}
 
 	@Test
-	public void evaluateBuzz() {
+	public void whenMultiple5ReturnBuzz() {
 		assertEquals("Buzz", fizzBuzz.evaluate(5));
 	}
 
 	@Test
-	public void valuateFizzBuzz() {
+	public void whenMultiple3And5ReturnFizzBuzz() {
 		assertEquals("FizzBuzz", fizzBuzz.evaluate(15));
+	}
 
+	@Test
+	public void whenListOfInteger() {
+		for(int index = 0; index < values.length; index++){
+			assertEquals(values[index], fizzBuzz.evaluate(index+1));
+		}
 	}
 
 }
